cb8e33c40a25a4ead900900537b5a321c7f5b77a
Rogier Schaap <reschaap@xs4all.nl>
2013-10-17 23:32:57 +0200

    Added and modified functions
    Added corridor()
    	modified:   ex36.py

--- ex36.py
+++ ex36.py
@@ -1,4 +1,3 @@
-# as example only 
 from sys import exit
 
 def start_room():
@@ -10,8 +9,7 @@ def start_room():
     print """All of a sudden there is a crash. You are pushed out of
     your chair and you head hits the bulk head."""
     print """You open your eyes and look around. The lights have dimmed.\n"""
-    
-    
+        
     print "What will you do?"
     print "Stay here. 1)"
     print "Go to the corridor. 2)\n" 
@@ -20,12 +18,11 @@ def start_room():
     if int(next) == 1:
         death()
     elif int(next) == 2:
-        print "You choose 2"
+        corridor()
     else:
         print "Wrong choice"
-    
-    exit(0)
-        
+        exit(0)
+
 
 def death():
     print "You choose wrongly"
@@ -37,5 +34,30 @@ def death():
     else:
         exit(0)
 
-start_room()
 
+def corridor():
+    print "You leave your room and look around. You notice it is as dark as it\
+ was back in the room. The only light comes the red emergency lights."
+    print "On your right you see the escape pod while in front of you there is\
+ the door to engineering"
+    
+    print "\nWhat will you do?"
+    print "Go back to your room. 1)"
+    print "Go to the escape pod. 2)"
+    print "Go to engineering. 3)"
+    next = raw_input(">... ")
+    
+    if int(next) == 1:
+        death()
+    elif int(next) == 2:
+        print "You choose 2"
+        #escape_pod()
+    elif int(next) == 3:
+        print "You choose 3"
+        #engineering()
+    else:
+        print "Wrong choice"
+        exit(0)
+
+
+start_room()
